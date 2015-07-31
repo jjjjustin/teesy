b3d8a678dd967ed3339a380423c2e1bacfaf1ef5
jjjjustin <justinsitarz@gmail.com>
2015-07-31 03:41:20 -0500

    color filtering added

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,38 +3,24 @@ class SearchController < ApplicationController
 
   def index
     query = params.fetch(:query, 'crew')
-    color = params.fetch(:color, 'BLACK')
-    url = "http://temporaryshirts.herokuapp.com/query?q=#{query}&limit=50&page=0"
+    search_color = params.fetch(:color, 'WHITE')
+    page = params.fetch(:page, 0)
+    black = '"' + search_color + '"'
+    filter = '[{"color":' + "#{black}}]"
+    sort = '[("price",-1)]'
+    url = "http://temporaryshirts.herokuapp.com/query?q=#{query}&limit=25&page=#{page}&filters=#{filter}"
     response = JSON.parse(open(url).read)
     @items = response
-    @itemcodes = @items.map { |item| JSON.parse(open(itemcode_url(item['code'])).read) }
+    @item_codes = response.map { |item| JSON.parse(open(item_code_url(item['code'])).read) }
+
   end
 
-  def itemcode_url(code)
+  def item_code_url(code)
     "http://temporaryshirts.herokuapp.com/itemcode?code=#{code}"
   end
 
 end
 
 
-
-# helper_method :img_url_for_item
-
-#   def index
-#     query = params.fetch(:query, 'wood')
-#     url = "https://openapi.etsy.com/v2/listings/active?api_key=pmpusjqr5lyr7alo7o1x4lh4&keywords=#{query}&limit=12"
-#     response = JSON.parse(open(url).read)
-#     @items = response['results']
-#     @images = response['results'].map { |item| JSON.parse(open(image_url(item['listing_id'])).read) }
-#   end
-
-#   def image_url(id)
-#     "https://openapi.etsy.com/v2/listings/#{id}/images?api_key=pmpusjqr5lyr7alo7o1x4lh4"
-#   end
-
-#   def img_url_for_item(item)
-#     @images.find { |img| img.fetch("results").first.fetch("listing_id") == item["listing_id"] }["results"].first["url_170x135"]
-#   end
-# end
 
 
